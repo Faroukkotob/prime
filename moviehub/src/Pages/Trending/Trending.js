8f56dbcad390782ab54d817cdfc04c9b59877f99
farouk <farouk.2002.kotob@gmail.com>
2023-12-17 23:32:21 -0500

    Trending Api Setup (moviedb Api)

--- moviehub/src/Pages/Trending/Trending.js
+++ moviehub/src/Pages/Trending/Trending.js
@@ -1,7 +1,40 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import '../../Components/SingleContent/SignleContent'
+import SingleContent from "../../Components/SingleContent/SignleContent";
 const Trending = () => {
+    const [content, setContent] = useState([]);
+
+    const fetchTrending = async () => {
+
+        const {data} = await axios.get(
+            `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+        );
+        setContent(data.results);
+        
+    };
+
+    useEffect(() => {
+        fetchTrending();
+    },[]);
     return (
         <div>
             <span className="pageTitle">Trending</span>
+            <div className='trending'>
+                {
+                    content && content.map((c) => (
+                        <SingleContent 
+                        key={c.id} 
+                        id={c.id} 
+                        poster={c.poster_path} 
+                        title={c.title || c.name} 
+                        date={c.first_air_data || c.release_date}
+                        media_type={c.media_type}
+                        vote_avaerge= {c.vote_avaerge}
+                        />
+                    ))
+                }
+            </div>
         </div>
     )
 }
