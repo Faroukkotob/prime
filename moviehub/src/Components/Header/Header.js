1425482c2fc60ad721606e7323295d3268d94e4b
farouk <farouk.2002.kotob@gmail.com>
2023-12-18 21:15:59 -0500

    minor edits

--- moviehub/src/Components/Header/Header.js
+++ moviehub/src/Components/Header/Header.js
@@ -1,7 +1,7 @@
 import './Header.css';
 
 const Header = () => {
-    return <span className="header"> Movie Hub  🎥</span>
+    return <span className="header"> Cinemate  🎥</span>
 };
 
 export default Header;
